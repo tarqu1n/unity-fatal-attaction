d69d622181bb0d519b7a61b9d1c917e012cffb5e
matt.langley <matt.tarquin@gmail.com>
2019-09-15 15:27:34 +0100

    initial commit.

---
+++ Assets/RandomPatrol.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RandomPatrol : MonoBehaviour
+{
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    public float minSpeed;
+    public float maxSpeed;
+
+    float speed;
+
+    public float secondsToMaxDifficulty;
+
+    Vector2 targetPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = GetRndPosWithinBounds();
+        speed = minSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Vector2)transform.position != targetPosition)
+        {
+            speed = Mathf.Lerp(minSpeed, maxSpeed, GetDifficultyPercent());
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+        else
+        {
+            targetPosition = GetRndPosWithinBounds();
+        }
+    }
+
+    Vector2 GetRndPosWithinBounds()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+
+        return new Vector2(randomX, randomY);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Monster")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    float GetDifficultyPercent()
+    {
+        return Mathf.Clamp01(Time.timeSinceLevelLoad / secondsToMaxDifficulty);
+    }
+}
